4aff2496a24867cd1a14dec82e74c367af83222b
Karan Singh Saini <karansingh7613@gmail.com>
2019-04-10 18:43:54 +0530

    First Commit

--- src/Components/audioplayer.js
+++ src/Components/audioplayer.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
 import '../style.css';
 import noBodyCanSaveMeNow from "../LP/1.mp3";
 import goodBye from "../LP/2.mp3";
+import ttm from "../LP/3.mp3";
+import bs from "../LP/4.mp3"
+import invisible from "../LP/5.mp3";;
 
 function getTime(time) {
   if (!isNaN(time)) {
@@ -42,6 +46,15 @@ class Musicplayer extends Component {
         case "Good Goodbye":
           track = goodBye;
           break;
+        case "Talking To Myself":
+          track = ttm;
+          break;
+        case "Battle Symphony":
+          track = bs;
+          break;
+          case "Invisible":
+            track = invisible;
+          break;
         default:
           break;
       }
@@ -71,7 +84,10 @@ class Musicplayer extends Component {
   render() {
     const list = [
       { id: 1, title: "Nobody can save me now" },
-      { id: 2, title: "Good Goodbye" }
+      { id: 2, title: "Good Goodbye" },
+      { id: 3, title: "Talking To Myself" },
+      { id: 4, title: "Battle Symphony" },
+      { id: 5, title: "Invisible" }
     ].map(item => {
       return (
         <li
@@ -89,29 +105,30 @@ class Musicplayer extends Component {
     return (
       <>
       <div className="player">
-        <h1>Heaven Player</h1>
-        <ul>{list}</ul>
-        <div>
+        <h1 className = "title">Heaven Player</h1>
+        <div className="cover" />
+        <ul className="songs">{list}</ul>
+        <div className="btnplay">
           {this.state.player === "paused" && (
-            <button onClick={() => this.setState({ player: "playing" })}>
+            <Button onClick={() => this.setState({ player: "playing" })}>
               Play
-            </button>
+            </Button>
           )}
           {this.state.player === "playing" && (
-            <button onClick={() => this.setState({ player: "paused" })}>
+            <Button variant="light" onClick={() => this.setState({ player: "paused" })}>
               Pause
-            </button>
+            </Button>
           )}
           {this.state.player === "playing" || this.state.player === "paused" ? (
-            <button onClick={() => this.setState({ player: "stopped" })}>
+            <Button variant="light" onClick={() => this.setState({ player: "stopped" })}>
               Stop
-            </button>
+            </Button>
           ) : (
             ""
           )}
         </div>
         {this.state.player === "playing" || this.state.player === "paused" ? (
-          <div>
+          <div className="time">
             {currentTime} / {duration}
           </div>
         ) : (
